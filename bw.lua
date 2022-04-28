055b060998965e92c23b65b1515065334662faf3
SkyLi000 <97918490+SkyLi000@users.noreply.github.com>
2022-04-28 19:58:24 +1200

    Update bw.lua

--- bw.lua
+++ bw.lua
@@ -1331,7 +1331,7 @@ local GUIbind = GUI.CreateGUIBind()
 
 local teleportfunc = game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
     if State == Enum.TeleportState.Started and not shared.VapeIndependent then
-		local teleportstr = 'shared.VapeSwitchServers = true if shared.VapeDeveloper then loadstring(readfile("vape/NewMainScript.lua"))() else loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))() end'
+		local teleportstr = 'shared.VapeSwitchServers = true if shared.VapeDeveloper then loadstring("https://raw.githubusercontent.com/SkyLi000/TestScript/main/bw.lua")() else loadstring(game:HttpGet("https://raw.githubusercontent.com/SkyLi000/TestScript/main/bw.lua", true))() end'
 		if shared.VapeDeveloper then
 			teleportstr = 'shared.VapeDeveloper = true '..teleportstr
 		end
